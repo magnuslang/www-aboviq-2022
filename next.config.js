55134ff4a785e689c52205f8ad3c3c4236421781
Magnus Lång <magnus@aboviq.com>
2022-03-16 11:05:38 +0100

    feat(analytics): add Splitbee analytics

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/bee.js',
+          destination: 'https://cdn.splitbee.io/sb.js',
+        },
+        {
+          source: '/_hive/:slug',
+          destination: 'https://hive.splitbee.io/:slug',
+        },
+      ],
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/header.css';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script data-api="/_hive" src="/bee.js" strategy="beforeInteractive" />
+      <Component {...pageProps} />
+    </>
+  );
 }
