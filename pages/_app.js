8c650eafb0796f7f3abd5575dfe78abc74ea2808
Magnus Lång <magnus@aboviq.com>
2022-03-16 11:46:47 +0100

    feat(analytics): no rewrite

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script data-api="/_hive" src="/bee.js" data-no-cookie />
+      <Script src="https://cdn.splitbee.io/sb.js" data-no-cookie />
       <Component {...pageProps} />
     </>
   );
